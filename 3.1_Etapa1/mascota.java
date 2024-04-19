4d0f6ff4a785a47a7d520b0224aa973a45a5cdba
CerberoCL <martin.alonso@usm.cl>
2024-04-19 18:58:25 -0400

    Parte 1 Lista

--- 3.1_Etapa1/mascota.java
+++ 3.1_Etapa1/mascota.java
@@ -15,140 +15,49 @@ public class Mascota {
             this.felicidad = 50;
             this.estado = Estado.MEH;
         }
-    
-        // Getters y setters
-    
-        public String getNombre() {
-            return nombre;
-        }
-    
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-    
-        public float getEdad() {
-            return edad;
-        }
-    
-        public void setEdad(float edad) {
-            this.edad = edad;
-        }
-    
-        public int getSalud() {
-            return salud;
-        }
-    
-        public void setSalud(int salud) {
-            this.salud = salud;
-        }
-    
-        public int getEnergia() {
-            return energia;
-        }
-    
-        public void setEnergia(int energia) {
-            this.energia = energia;
-        }
-    
-        public int getFelicidad() {
-            return felicidad;
-        }
-    
-        public void setFelicidad(int felicidad) {
-            this.felicidad = felicidad;
-        }
-    
-        public Estado getEstado() {
-            return estado;
-        }
-    
-        public void setEstado(Estado estado) {
-            this.estado = estado;
-        }
-    
-        // Métodos para actualizar los indicadores
-    
-        public void actualizarSalud(int deltaSalud) {
-            salud = Math.max(0, salud + deltaSalud);
-            actualizarEstado();
-        }
-    
-        public void actualizarEnergia(int deltaEnergia) {
-            energia = Math.max(0, energia + deltaEnergia);
-            actualizarEstado();
-        }
-    
-        public void actualizarFelicidad(int deltaFelicidad) {
-            felicidad = Math.max(0, felicidad + deltaFelicidad);
-            actualizarEstado();
-        }
-    
-        // Método para actualizar el estado
-    
-        private void actualizarEstado() {
-            if (edad <= 5 && salud <= 10) {
-                felicidad -= 20;
-            } else if (edad <= 10 && salud <= 50) {
-                felicidad -= 20;
-                energia -= 10;
-            } else if ( edad>10 && salud <= 50) {
-                felicidad -= 30;
-                energia -= 20;
-            }
-    
-            if (felicidad >= 60) {
-                estado = Estado.WEEEH; // feliz
-            } else if (felicidad <= 20) {
-                estado = Estado.SNIF; // triste
 
-            } else if (salud <= 20 && edad <= 5) {
-                estado = Estado.HAMBRE; //hambriento
 
-            } else if (salud <= 50 && edad <= 10 && edad>=5){
-                    estado= Estado.HAMBRE; //hambriento
-        
-            } else if (salud <= 30 && edad > 5 && energia <= 30) {
-                estado = Estado.GRRRR; //enojado
-
-            } else if (energia <= 15) {
-                estado = Estado.CANSADO; //cansado
-            
-            } else if (energia == 0 || salud==0 || edad>=15) {
-                estado = Estado.MUERTO; //muerto
-
-            } else {
-                estado = Estado.MEH;
-            }
-        }
-    
-        // Método para dormir
-    
-        public void dormir() {
-            energia = 100;
-            salud += 15;
-            felicidad += 15;
-            if(salud >= 100) {
-                salud = 100;
-            } 
-            if(energia >= 100){
-                energia = 100;
-            }
-            actualizarEstado();
-        }
-    
-        // Método para mostrar información de la mascota
-    
         @Override
-public String toString() {
-    return "Mascota{" +
-            "\n nombre='" + nombre + '\'' +
-            ",\n edad=" + edad +
-            ",\n salud=" + salud +
-            ",\n energia=" + energia +
-            ",\n felicidad=" + felicidad +
-            ",\n estado=" + estado +
-            "\n}";
-}
+        public String toString(){
+            String estadoString;
+            switch (estado){
+                case MEH:
+                    estadoString = "(-__-) Meh....";
+                   break;
+                case WEEEH:
+                    estadoString = "\\(^_^)/ Weeeeeh!";
+                    break;
+                case SNIF:
+                    estadoString = "(._.) snif....";
+                    break;
+                case HAMBRE:
+                    estadoString = "(OoO) hambre hambre!";
+                    break;
+                case GRRRR:
+                    estadoString = "(ôwô) grrrr....";
+                    break;
+                case CANSADO:
+                    estadoString = "(=_=) zzzz....";
+                    break;
+                case MUERTO:
+                    estadoString = "(x.x) fin del juego";
+                    break;
+                default:
+                    estadoString = "Estado desconocido";
+                    break;
+            }
+            return "Mascota Virtual" +
+                    "\n" +
+                    "\nAtributos" +
+                    "\n---------" +
+                    "\nnombre = " + nombre +
+                    "\nedad = " + edad +
+                    "\nsalud = " + salud +
+                    "\nenergia = " + energia +
+                    "\nfelicidad = " + felicidad +
+                    "\nestado = " + estadoString +
+                    "\n";
+        }
 }
     
     
